c08ce9c45e9219698081315913d450e18c43d863
Nikhil Kumar <nkumar.pro@gmail.com>
2025-04-15 14:20:44 -0400

    Fix package versions

---
+++ next.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  basePath: '/5x5_web',
+  images: {
+    unoptimized: true,
+  },
+}
+
+module.exports = nextConfig 
