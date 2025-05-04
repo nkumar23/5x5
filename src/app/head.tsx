27242e8b42db472885cd25dc65f61d7f8bf14d80
Nikhil Kumar <nkumar.pro@gmail.com>
2025-05-04 12:52:53 -0400

    favicon update

---
+++ src/app/head.tsx
@@ -0,0 +1,9 @@
+export default function Head() {
+  return (
+    <>
+      <title>5x5 Studios</title>
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <link rel="icon" href="/favicon.ico" type="image/x-icon" />
+    </>
+  );
+} 
